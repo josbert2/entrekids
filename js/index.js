441dd5d8681cf2b6254c15539852b88c2d42396a
josbert2 <hernandezjosbert@gmail.com>
2018-08-30 07:48:08 -0430

    fixed table

--- js/index.js
+++ js/index.js
@@ -1,5 +1,47 @@
 'use strict';
 
+/*=========================================================================
+   Croppie
+=========================================================================*/
+
+$(document).ready(function() {
+    var basic = $('#main-cropper').croppie({
+        viewport: {
+            width: 250,
+            height: 250
+        },
+        boundary: {
+            width: 300,
+            height: 300
+        },
+        showZoomer: false,
+        url: '../img/placeholder.png'
+    });
+
+    function readFile(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('#main-cropper').croppie('bind', {
+                    url: e.target.result
+                });
+                $('.actionDone').toggle();
+                $('.actionUpload').toggle();
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('.actionUpload input').on('change', function() {
+        readFile(this);
+    });
+    $('.actionDone').on('click', function() {
+        $('.actionDone').toggle();
+        $('.actionUpload').toggle();
+    })
+})
 
 
 /*=========================================================================
